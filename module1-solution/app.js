a4da0625b1b2b9c528a11d18f21bfcff36e6d644
LiveCurious <dewill666@gmail.com>
2017-11-11 21:38:19 +0200

    implemented the task

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,7 +3,29 @@
 {
   'use strict';
 
-  angular.module('myFirstApp', [])
+  angular.module('LunchCheck', [])
+  .controller('LunchController', LunchController);
 
-  .controller('myFirstController', function (){});
+LunchController.$inject = ['$scope'];
+function LunchController($scope)
+{
+$scope.value="";
+$scope.result = "";
+
+
+  $scope.Check = function()
+      {
+        var result ="";
+            var array  =  $scope.value.split(',');
+                if(array.length >3)
+                {
+                  result = "Too much";}
+                else {
+                  result= "We are good";
+                }
+    $scope.result= result;
+    }
+}
+
+
 }());
